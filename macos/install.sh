4c7074f20a4dc9e04125dc341a4555b0e8e60a36
Jacob Pledger <jacobpledger@Jacobs-MBP.hitronhub.home>
2017-12-25 16:34:07 -0700

    added a bunch of defaults writes

--- macos/install.sh
+++ macos/install.sh
@@ -8,16 +8,92 @@
 # Reduce Transparency
 #defaults write com.apple.universalaccess reduceTransparency -bool true
 
+# Trackpad
+
+defaults write com.apple.AppleMultitouchTrackpad ActuateDetends -int 1
+defaults write com.apple.AppleMultitouchTrackpad DragLock -bool false
+defaults write com.apple.AppleMultitouchTrackpad Dragging -bool false
+defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 1
+defaults write com.apple.AppleMultitouchTrackpad ForceSuppressed -bool false
+defaults write com.apple.AppleMultitouchTrackpad SecondClickThreshold -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFiveFingerPinchGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHoizSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerPinchGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerVertSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadHandResting -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadHorizScroll -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadMomentumScroll -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadPinch -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRotate -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadScroll -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerDoubleTapGesture -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 3
+defaults write com.apple.AppleMultitouchTrackpad USBMouseStopsTrackpad -int 1
+defaults write com.apple.AppleMultitouchTrackpad UserPreferences -bool true
+
+# Finder Preferences
+
+## New Finder windows show Home folder
+defaults write com.apple.finder NewWindowTarget PfHm
+
+# Sort Folders First
+defaults write com.apple.finder _FXSortFoldersFirst -bool YES
+
+## Desktop Icons
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+
+# Lock Screen
+## Require password after screensaver
+defaults write com.apple.screensaver askForPassword -int 1
+## Ask for password after n seconds
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+
+# Dock
+
+# Set minimize effect
+defaults write com.apple.dock mineffect -string suck
+# defaults write com.apple.dock mineffect -string scale
+# defaults write com.apple.dock mineffect -string genie
+
+# Hot corners -> bottom left -> start screen saver
+defaults write com.apple.dock "wvous-bl-corner" -int 5
+defaults write com.apple.dock "wvous-bl-modifier" -int 0
+
+# Set natural scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+
+# 24-Hour Time
+defaults write NSGlobalDomain AppleICUForce12HourTime -bool true
+
+# Use smooth scrolling
+defaults write NSGlobalDomain AppleScrollAnimationEnabled -bool true
+
+# No translucent menu bar
+defaults write NSGlobalDomain "AppleEnableMenuBarTransparency" -bool false
+
 # Disable Auto Rearrange Spaces Based on Most Recent Use
 defaults write com.apple.dock mru-spaces -bool false && killall Dock
 
 # Set plaintext as default for TextEdit
 defaults write com.apple.TextEdit RichText -int 0
 
+
 # Disable Infrared Receiver
 defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -int 0
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install tmux
-brew cask install calibre firefox textmate tunnelblick
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew install tmux
+# brew cask install calibre firefox nextcloud sublime-text textmate tunnelblick
+
+killall Dock
