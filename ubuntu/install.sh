555c01ea08e1e80633013a952d17bf6d0d3ef3ce
Jacob Pledger <jacob@jacobpledger.com>
2018-05-29 14:11:37 -0600

    remove onboard

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,7 +4,9 @@ icon_dir="$HOME/.icons/"
 
 sudo apt install -y git
 
-# remove junk
+# ******************************************************************************
+# Remove Junk
+# ******************************************************************************
 sudo apt remove -y \
 	aisleriot brasero-* \
 	cheese \
@@ -24,6 +26,8 @@ sudo apt remove -y \
 	inkscape \
 	libreoffice \
 	lightsoff \
+	onboard \
+	onboard-data \
 	parole \
 	sgt-puzzles \
 	simple-scan \
@@ -34,36 +38,42 @@ sudo apt remove -y \
 	xfburn \
 	xfce4-notes
 
-# install snaps for trendyness
+# ******************************************************************************
+# Snaps
+# ******************************************************************************
 sudo snap install canonical-livepatch
 sudo snap install --classic slack
 sudo snap install docker
 sudo snap install keepassxc
+sudo snap install nextcloud-client
 
-# add repos
+# ******************************************************************************
+# Add Repos
+# ******************************************************************************
 
 # sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-# nextcloud
-sudo add-apt-repository ppa:nextcloud-devs/client
 
 # numix theme and icons
 sudo add-apt-repository ppa:numix/ppa
 
 sudo apt update
 
+# ******************************************************************************
 # install packages
+# ******************************************************************************
 
 # install separately so failures don't cascade
 
-# sudo apt install -y evolution
-# sudo apt install -y evolution-ews
+sudo apt install -y apt-transport-https # for sublime
+sudo apt install -y evolution
+sudo apt install -y evolution-ews
 #sudo apt install -y firefox
-sudo apt install -y nextcloud-client
 sudo apt install -y numix-gtk-theme
 sudo apt install -y numix-icon-theme-circle
+sudo apt install -y openssh-server
+sudo apt install -y openssh-sftp-server 
 sudo apt install -y plank
 sudo apt install -y sublime-text
 sudo apt install -y tmux
@@ -75,7 +85,9 @@ sudo apt autoremove
 # rust
 curl https://sh.rustup.rs -sSf | sh
 
-# customization
+# ******************************************************************************
+# Customization
+# ******************************************************************************
 mkdir -p $HOME/Development/Workflow
 git clone https://github.com/LinxGem33/Plank-Themes.git $HOME/Development/Workflow/
 
